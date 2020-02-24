085271abeb2369dc18ba8f7254bbb9b7e663161f
CBreakr <github email address>
2020-02-24 13:23:39 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+
+    attr_reader :name
+    attr_accessor :owner, :mood
+
+    @@all = []
+
+    def initialize(cat_name, owner)
+      @name = cat_name
+      @owner = owner
+      @mood = "nervous"
+      Cat.all << self
+    end
+
+    def self.all
+      @@all
+    end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(dog_name, owner)
+    @name = dog_name
+    @owner = owner
+    @mood = "nervous"
+    Dog.all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
